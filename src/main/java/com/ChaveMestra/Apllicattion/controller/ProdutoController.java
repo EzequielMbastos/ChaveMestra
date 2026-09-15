777967b4e65ae036b1f60ddc313af00d364e97fd
EzequielMBastos <ezequielmbastos@gmail.com>
2026-09-15 16:40:56 -0300

    criado ProdutoController.java

--- src/main/java/com/ChaveMestra/Apllicattion/controller/ProdutoController.java
+++ src/main/java/com/ChaveMestra/Apllicattion/controller/ProdutoController.java
@@ -1,4 +1,33 @@
 package com.ChaveMestra.Apllicattion.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.ChaveMestra.Apllicattion.model.Produto;
+import com.ChaveMestra.Apllicattion.service.ProdutoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService)
+    {
+        this.produtoService = produtoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> cadastrar(@RequestBody Produto produto) {
+        Produto produtoSalvo = produtoService.cadastrar(produto);
+        return ResponseEntity.ok(produtoSalvo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> listar()
+    {
+        return ResponseEntity.ok(produtoService.listar());
+    }
 }
