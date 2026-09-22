4fe12316bd789bee94cb07c309888e52c25af670
EzequielMBastos <ezequielmbastos@gmail.com>
2026-09-22 09:53:31 -0300

    feat: implementa CRUD completo de CategoriaFinanceiraController

--- src/main/java/com/ChaveMestra/Apllicattion/controller/CategoriaFinanceiraController.java
+++ src/main/java/com/ChaveMestra/Apllicattion/controller/CategoriaFinanceiraController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/CategoriaFinanceira")
+@RequestMapping("/categoria-financeira")
 public class CategoriaFinanceiraController {
 
     private final CategoriaFinanceiraService categoriaFinanceiraService;
---
+++ src/main/java/com/ChaveMestra/Apllicattion/dto/AtendimentoItemRequest.java
@@ -0,0 +1,5 @@
+package com.ChaveMestra.Apllicattion.dto;
+
+public record AtendimentoItemRequest() {
+
+}
