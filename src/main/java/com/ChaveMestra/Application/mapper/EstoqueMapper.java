960d0ba838ac7438e7ffb31a7471c6852eee2427
EzequielMBastos <ezequielmbastos@gmail.com>
2026-09-22 14:18:16 -0300

    feat(estoque): implementa EstoqueMapper.java

--- src/main/java/com/ChaveMestra/Application/mapper/EstoqueMapper.java
+++ src/main/java/com/ChaveMestra/Application/mapper/EstoqueMapper.java
@@ -1,4 +1,34 @@
 package com.ChaveMestra.Application.mapper;
 
+import com.ChaveMestra.Application.dto.EstoqueRequest;
+import com.ChaveMestra.Application.dto.EstoqueResponse;
+import com.ChaveMestra.Application.model.Estoque;
+import com.ChaveMestra.Application.model.Produto;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EstoqueMapper {
+
+
+    public EstoqueResponse toResponse(Estoque estoque) {
+        return new EstoqueResponse(
+                estoque.getId(),
+                estoque.getProduto() != null ? estoque.getProduto().getId() : null,
+                estoque.getProduto() != null ? estoque.getProduto().getNome() : null,
+                estoque.getQuantidade(),
+                estoque.getMinimo(),
+                estoque.getQuantidade() <= estoque.getMinimo(),  // ← calculado
+                estoque.getDtEntrada(),
+                estoque.getDtAtualizacao()
+        );
+    }
+        public Estoque toEntity (EstoqueRequest request, Produto produto){
+            Estoque estoque = new Estoque();
+            estoque.setProduto(produto);
+            estoque.setQuantidade(request.quantidade());
+            estoque.setMinimo(request.minimo());
+            // dtEntrada e dtAtualizacao NÃO são setados → @PrePersist preenche
+            return estoque;
+        }
 }
+
