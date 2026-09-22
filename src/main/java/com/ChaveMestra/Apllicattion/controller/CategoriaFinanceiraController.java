4d5e34fabf5e3162e48b5c9d9e11a5d6454ab256
EzequielMBastos <ezequielmbastos@gmail.com>
2026-09-22 09:33:50 -0300

    feat: implementa CRUD completo de CategoriaFinanceiraController

--- src/main/java/com/ChaveMestra/Apllicattion/controller/CategoriaFinanceiraController.java
+++ src/main/java/com/ChaveMestra/Apllicattion/controller/CategoriaFinanceiraController.java
@@ -1,4 +1,53 @@
 package com.ChaveMestra.Apllicattion.controller;
 
+import com.ChaveMestra.Apllicattion.dto.CategoriaFinanceiraRequest;
+import com.ChaveMestra.Apllicattion.dto.CategoriaFinanceiraResponse;
+import com.ChaveMestra.Apllicattion.model.CategoriaFinanceira;
+import com.ChaveMestra.Apllicattion.service.CategoriaFinanceiraService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/CategoriaFinanceira")
 public class CategoriaFinanceiraController {
+
+    private final CategoriaFinanceiraService categoriaFinanceiraService;
+
+    public CategoriaFinanceiraController(CategoriaFinanceiraService categoriaFinanceiraService) {
+        this.categoriaFinanceiraService = categoriaFinanceiraService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoriaFinanceiraResponse> cadastrar(@RequestBody CategoriaFinanceiraRequest request ){
+        CategoriaFinanceiraResponse categoriaFinanceiraSalva = categoriaFinanceiraService.cadastrar(request);
+        return ResponseEntity.ok(categoriaFinanceiraSalva);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaFinanceiraResponse>> listar(){
+        return ResponseEntity.ok(categoriaFinanceiraService.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaFinanceiraResponse> buscarPorId(@PathVariable Integer id) {
+        CategoriaFinanceiraResponse categoriaFinanceira = categoriaFinanceiraService.buscarPorId(id);
+        return ResponseEntity.ok(categoriaFinanceira);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+        categoriaFinanceiraService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoriaFinanceiraResponse> atualizar(@PathVariable Integer id, @RequestBody CategoriaFinanceiraRequest request) {
+        CategoriaFinanceiraResponse categoriaFinanceiraAtualizada = categoriaFinanceiraService.atualizar(id, request);
+        return ResponseEntity.ok(categoriaFinanceiraAtualizada);
+    }
 }
+
+
