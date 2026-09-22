ff3b08053e379ddcb833a085e5b42a2969b5daa3
EzequielMBastos <ezequielmbastos@gmail.com>
2026-09-22 09:34:15 -0300

    feat: implementa CRUD completo de FornecedorService

--- src/main/java/com/ChaveMestra/Apllicattion/service/FornecedorService.java
+++ src/main/java/com/ChaveMestra/Apllicattion/service/FornecedorService.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-public class FornecedorService {
+public class FornecedorService{
 
     private final FornecedorRepository fornecedorRepository;
     private final FornecedorMapper fornecedorMapper;
