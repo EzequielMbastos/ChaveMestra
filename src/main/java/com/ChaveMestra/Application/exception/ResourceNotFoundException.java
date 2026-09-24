bb65fd02f9f0628f035e0baaa8ac46e99f1b9af9
EzequielMBastos <ezequielmbastos@gmail.com>
2026-09-24 17:11:31 -0300

    Adiciona tratamento global de excecoes

---
+++ src/main/java/com/ChaveMestra/Application/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ChaveMestra.Application.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
